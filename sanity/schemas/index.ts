9c24a5959b7acef253a76ad44ed44d01f78eb2fc
Ali Raza <alirazaprince332@gmail.com>
2026-08-16 10:24:02 +0500

    Create index.ts to export schema types

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,21 @@
+import classSchema from './class'
+import subjectSchema from './subject'
+import chapterSchema from './chapter'
+import topicSchema from './topic'
+import assessmentSchema from './assessment'
+import staffSchema from './staff'
+import studentSchema from './student'
+import enrollmentSchema from './enrollment'
+import progressSchema from './progress'
+
+export const schemaTypes = [
+  classSchema,
+  subjectSchema,
+  chapterSchema,
+  topicSchema,
+  assessmentSchema,
+  staffSchema,
+  studentSchema,
+  enrollmentSchema,
+  progressSchema
+]
